b5b46b4cd38feff9c6a0acdcded6c8499c47096d
maria Daniella <mariadanigikonyo@gmail.com>
2019-07-21 19:19:33 +0300

    add jquery function to what we do

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,33 @@
 $(document).ready(function(){
   $("#development-image").click(function(){
-    $("#development-image").slideDown('1500').hide('1000');
-    $("#development").show('1500');
+    $("#development-image").slideDown('1000').hide('1000');
+    $("#development").show('1000');
   });
   $("#development").click(function(){
-    $("#development").slideUp('1500');
-    $("#development-image").slideDown('1500');
+    $("#development").slideUp('1000');
+    $("#development-image").slideDown('1000');
   });
 });
 
 $(document).ready(function(){
   $("#design-image").click(function(){
-    $("#design-image").slideDown('1500').hide('1000');
-    $("#design").show('1500');
+    $("#design-image").slideDown('1000').hide('1000');
+    $("#design").show('1000');
   });
   $("#design").click(function(){
-    $("#design").slideUp('1500');
-    $("#design-image").slideDown('1500');
+    $("#design").slideUp('1000');
+    $("#design-image").slideDown('1000');
   });
 });
 
 $(document).ready(function(){
   $("#product-image").click(function(){
-    $("#product-image").slideDown('1500').hide('1000');
-    $("#product").show('1500');
+    $("#product-image").slideDown('1000').hide('1000');
+    $("#product").show('1000');
   });
   $("#product").click(function(){
-    $("#product").slideUp('1500');
-    $("#product-image").slideDown('1500');
+    $("#product").slideUp('1000');
+    $("#product-image").slideDown('1000');
   });
 });
 $(document).ready(function(){

cc9b9889ff9001a369808070ee5e83d8d88e2e9e
maria Daniella <mariadanigikonyo@gmail.com>
2019-07-22 08:20:52 +0300

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+ls
+ $(document).ready(function(){
   $("#development-image").click(function(){
     $("#development-image").slideDown('1000').hide('1000');
     $("#development").show('1000');
@@ -79,5 +80,19 @@ $(document).ready(function(){
     $("#overlay8").show();
   }).mouseout(function(){
     $("#overlay8").hide();
+  });
+});
+$(document).ready(function(){
+  $("form#contact").submit(function(event){
+    event.preventDefault();
+    var name = $("input#MERGE1").val();
+    var email = $("input#MERGE0").val();
+    var message = $("textarea#comment").val();
+    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+      alert (name + ", we have received your message. Thank you for reaching out to us.");
+    }
+    else {
+      alert("Please enter your name and email!");
+    }
   });
 });

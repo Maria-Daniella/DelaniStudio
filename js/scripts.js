e53b1f0cdd0b8e776f376766e205a9ab38b27b03
maria Daniella <mariadanigikonyo@gmail.com>
2019-07-22 10:38:20 +0300

    last changes

--- js/scripts.js
+++ js/scripts.js
@@ -5,37 +5,37 @@ $(document).ready(function(){
     alert(name +"We have received your message. Thank you for contacting us!")
   });
 });
- $(document).ready(function(){
-  $("#development-image").click(function(){
-    $("#development-image").slideDown('1000').hide('1000');
-    $("#development").show('1000');
-  });
-  $("#development").click(function(){
-    $("#development").slideUp('1000');
-    $("#development-image").slideDown('1000');
-  });
+$(document).ready(function(){
+   $("#development-image").click(function(){
+   $("#development-image").slideDown('1500').hide('1000');
+   $("#development").show('1500');
+ });
+ $("#development").click(function(){
+   $("#development").slideUp('1500');
+   $("#development-image").slideDown('1500');
+ });
+
+
+$(document).ready(function(){
+ $("#design-image").click(function(){
+   $("#design-image").slideDown('1500').hide('1000');
+   $("#design").show('1500');
+ });
+ $("#design").click(function(){
+   $("#design").slideUp('1500');
+   $("#design-image").slideDown('1500');
+ });
 });
 
 $(document).ready(function(){
-  $("#design-image").click(function(){
-    $("#design-image").slideDown('1000').hide('1000');
-    $("#design").show('1000');
-  });
-  $("#design").click(function(){
-    $("#design").slideUp('1000');
-    $("#design-image").slideDown('1000');
-  });
-});
-
-$(document).ready(function(){
-  $("#product-image").click(function(){
-    $("#product-image").slideDown('1000').hide('1000');
-    $("#product").show('1000');
-  });
-  $("#product").click(function(){
-    $("#product").slideUp('1000');
-    $("#product-image").slideDown('1000');
-  });
+ $("#product-image").click(function(){
+   $("#product-image").slideDown('1500').hide('1000');
+   $("#product").show('1500');
+ });
+ $("#product").click(function(){
+   $("#product").slideUp('1500');
+   $("#product-image").slideDown('1500');
+ });
 });
 $(document).ready(function(){
   $("#work1").mouseover(function(){
